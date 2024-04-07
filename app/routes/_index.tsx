31769af236d027090cbd2251391c89bada3ed460
pritamcn <you@example.com>
2024-04-07 21:18:32 +0530

    tailwind setup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,12 +6,14 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
-
+import { Button } from "~/components/ui/button"
 export default function Index() {
   return (
+    <div>
     <h1 className="text-3xl font-bold underline text-red-700">
     Hello world!
   </h1>
-
+  <Button variant='outline'>Click me</Button>
+  </div>
   );
 }
